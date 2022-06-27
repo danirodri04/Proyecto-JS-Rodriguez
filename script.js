11b8701b3ead9322f4ed178f976172ab01f390f2
Daniel Rodriguez <danielrodri2304@gmail.com>
2022-06-27 14:07:06 -0300

    Proyecto

---
+++ script.js
@@ -0,0 +1,29 @@
+//Comentarios de una linea
+/*
+Comentario multi-linea
+
+*/
+/**
+ * Comentario multi-linea: esto sirve para comentar algo mas complejo 
+ */
+
+/*Las palabras reservadas tienen un color unico*/
+
+var nombre = "Daniel";  //String o texto.
+var apellido = "Rodriguez";
+var sueldo = 1050 //Formato number o numero
+var esProfesor = true //Boolean o logicas es_profesor, son verdaderos o falsos este tipo de variables.
+var esPropietario = null //null o vacio, este es un dato vacio no tiene nada
+var esPadre = undefined //undefined o indefinido, no se sabe cual es el tipo de dato, java script usa mucho esto ya que trae muchos datos indefinidos ya que este dato lo defino yo como programador o usuairo.
+
+console.log(nombre)
+nombre = "Pancho"
+console.log(nombre)
+nombre = true
+console.log(nombre)
+nombre = null
+console.log(nombre)
+nombre= undefined
+console.log(nombre)
+
+//Las variables se pueden modificar facilmente//
